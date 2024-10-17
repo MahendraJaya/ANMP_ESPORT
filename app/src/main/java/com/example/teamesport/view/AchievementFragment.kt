8e21f353bef07555b861eb67f9b9ab01fcfcd783
MahendraJaya <hendraputra0645@gmail.com>
2024-10-17 09:41:10 +0700

    progress_achievement_by_year_done

--- app/src/main/java/com/example/teamesport/view/AchievementFragment.kt
+++ app/src/main/java/com/example/teamesport/view/AchievementFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -33,6 +34,7 @@ class AchievementFragment : Fragment() {
         viewModel = ViewModelProvider(this).get(AchievementViewModel::class.java)
         viewModel.refresh()
 
+        //untuk gambar
         val picasso = Picasso.Builder(view.context)
         picasso.listener { picasso, uri, exception ->
             exception.printStackTrace()
@@ -40,16 +42,30 @@ class AchievementFragment : Fragment() {
         val gambar = arguments?.getString("gameImage")
         picasso.build().load(gambar).into(binding.imgGame)
 
-
+        val tahun = arrayOf("All", "2019", "2020", "2021", "2022", "2023", "2024").toMutableList()
+        //untuk load achievement
         binding.txtTitle.text = arguments?.getString("gameName")
-        viewModel.achievementLD.observe(viewLifecycleOwner, Observer {
-            lastAchievement -> viewAchievement(lastAchievement, binding.txtTitle.text.toString())
-        })
-        val tahun = arrayOf("2019", "2020", "2021", "2022", "2023", "2024").toMutableList()
+
         val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, tahun)
         binding.spinnerAchievement.adapter = adapter
+        binding.spinnerAchievement.setSelection(0)
 
+        //untuk dropdown button atau spinner
 
+        binding.spinnerAchievement.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                val selectedTahun = tahun[position]
+                viewModel.achievementLD.observe(viewLifecycleOwner, Observer {
+                        lastAchievement -> viewAchievementWithYear(lastAchievement, binding.txtTitle.text.toString(),
+                    selectedTahun)
+                })
+
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                binding.txtAchievement.text = "No Achievement Found"
+            }
+        }
     }
 
     fun viewAchievement(achievement: List<Model.Achievement>, gameName: String){
@@ -61,4 +77,23 @@ class AchievementFragment : Fragment() {
         binding.txtAchievement.text = formattedAchievements
     }
 
+    fun viewAchievementWithYear(achievement: List<Model.Achievement>, gameName: String, year:String){
+
+        if(year == "All"){
+            val filteredAchievements = achievement.filter { it.gameTitle == gameName }
+            val formattedAchievements = filteredAchievements.mapIndexed { index, achievement ->
+                "${index + 1}. ${achievement.gameAchievement} (${achievement.gameYear})"
+            }.joinToString("\n")
+            binding.txtAchievement.text = formattedAchievements
+        }else{
+            val filteredAchievements = achievement.filter { (it.gameTitle == gameName) && (it.gameYear == year.toInt()) }
+            val formattedAchievements = filteredAchievements.mapIndexed { index, achievement ->
+                "${index + 1}. ${achievement.gameAchievement} (${achievement.gameYear})"
+            }.joinToString("\n")
+            binding.txtAchievement.text = formattedAchievements
+        }
+
+
+
+    }
 }
