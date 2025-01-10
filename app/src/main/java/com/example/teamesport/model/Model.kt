282d20e752b31d5e0ace4805c5669c594bda7fdb
Advenatrix <enders.sky@gmail.com>
2025-01-11 00:51:38 +0700

    dao

--- app/src/main/java/com/example/teamesport/model/Model.kt
+++ app/src/main/java/com/example/teamesport/model/Model.kt
@@ -11,39 +11,62 @@ import java.util.Date
 
 class Model {
 
+    @Entity(tableName = "games")
     data class Game(
+        @PrimaryKey(autoGenerate = true)
+        var id: Int = 0,
+        @ColumnInfo(name = "game_image_url")
         @SerializedName("gameImageUrl")
-        var gameImageUrl:String?,
+        var gameImageUrl: String?,
+        @ColumnInfo(name = "game_title")
         @SerializedName("gameTitle")
-        var gameTitle:String?,
+        var gameTitle: String?,
+        @ColumnInfo(name = "game_description")
         @SerializedName("gameDescription")
-        var gameDescription:String?
+        var gameDescription: String?
     )
 
+    @Entity(tableName = "achievements")
     data class Achievement(
+        @PrimaryKey(autoGenerate = true)
+        var id: Int = 0,
+        @ColumnInfo(name = "game_title")
         @SerializedName("gameTitle")
-        var gameTitle:String?,
+        var gameTitle: String?,
+        @ColumnInfo(name = "game_achievement")
         @SerializedName("gameAchievement")
-        var gameAchievement:String?,
+        var gameAchievement: String?,
+        @ColumnInfo(name = "game_year")
         @SerializedName("gameYear")
         var gameYear: Int?
     )
 
+    @Entity(tableName = "schedules")
     data class Schedule(
+        @PrimaryKey(autoGenerate = true)
+        var id: Int = 0,
+        @ColumnInfo(name = "sched_event")
         @SerializedName("schedEvent")
         var schedEvent: String,
+        @ColumnInfo(name = "sched_team")
         @SerializedName("schedTeam")
         var schedTeam: String?,
+        @ColumnInfo(name = "sched_time")
         @SerializedName("schedTime")
         var schedTime: String?,
+        @ColumnInfo(name = "sched_mon")
         @SerializedName("schedMon")
         var schedMon: String?,
+        @ColumnInfo(name = "sched_date")
         @SerializedName("schedDate")
         var schedDate: String?,
+        @ColumnInfo(name = "sched_desc")
         @SerializedName("schedDesc")
         var schedDesc: String?,
+        @ColumnInfo(name = "sched_location")
         @SerializedName("schedLocation")
         var schedLocation: String?,
+        @ColumnInfo(name = "sched_photo_url")
         @SerializedName("schedPhotoUrl")
         var schedPhotoUrl: String?
     )
